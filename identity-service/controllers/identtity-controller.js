8773d6734882b2008282c4ae863e0ca666b3ec32
Nguhope <jolienelgado96@gmail.com>
2026-03-21 13:36:21 +0100

    initial commit

--- identity-service/controllers/identtity-controller.js
+++ identity-service/controllers/identtity-controller.js
@@ -1,6 +1,25 @@
+ const logger = require('../utils/Logger')
+const { validateRegistration } = require('../utils/validation')
 //user registration
 
-const
+const registerUser = async (req,res) => {
+    logger.info('Registration endpoint hit....')
+
+
+
+    try {
+        const {} = validateRegistration(req.body)
+
+        if (error) {
+            logger.warn('Validation error', error.details[0].message  )
+            
+        }
+
+    } catch (error) {
+        
+    }
+    
+}
 
 
 //user login
